f22d8c677a1e69021e1efedbfac7ee83b9bd86e9
Morgan Houppin <morganhouppin@gmail.com>
2020-11-09 14:41:03 +0100

    Tuned LMR formula, epoch 200 Bench: 10274038

--- src/sources/engine/engine_go.c
+++ src/sources/engine/engine_go.c
@@ -34,7 +34,7 @@ void	init_reductions(void)
 {
 	for (int depth = 1; depth < 64; ++depth)
 		for (int count = 1; count < 64; ++count)
-			Reductions[depth][count] = 1.07 + log(depth) * log(count) / 1.51;
+			Reductions[depth][count] = 2.30 + log(depth) * log(count) / 3.65;
 }
 
 uint64_t	perft(board_t *board, unsigned int depth)
