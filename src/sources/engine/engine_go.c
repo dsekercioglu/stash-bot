aa3982c6b8d241824c967edc956c0ebfaf97ca39
Morgan Houppin <morganhouppin@gmail.com>
2020-11-12 19:58:01 +0100

    Thompson sampling, 250 epochs Bench: 7500862

--- src/sources/engine/engine_go.c
+++ src/sources/engine/engine_go.c
@@ -34,7 +34,7 @@ void	init_reductions(void)
 {
 	for (int depth = 1; depth < 64; ++depth)
 		for (int count = 1; count < 64; ++count)
-			Reductions[depth][count] = 2.48 + log(depth) * log(count) / 4.77;
+			Reductions[depth][count] = 1.16 + log(depth) * log(count) / 1.72;
 }
 
 uint64_t	perft(board_t *board, unsigned int depth)
