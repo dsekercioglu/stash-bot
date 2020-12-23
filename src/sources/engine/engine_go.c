e60e09741ae2197b893ea473e6971d0273bd15bb
Morgan Houppin <morganhouppin@gmail.com>
2020-12-23 18:36:33 +0100

    Tried a new method for LMR tuning, 20 epochs Bench: 19970113

--- src/sources/engine/engine_go.c
+++ src/sources/engine/engine_go.c
@@ -34,7 +34,7 @@ void        init_reduction_table(void)
 {
     for (int d = 1; d < 64; ++d)
         for (int m = 1; m < 64; ++m)
-            Reductions[d][m] = -0.48 + log(d) * log(m) / 1.3;
+            Reductions[d][m] = 0.18 + log(d) * log(m) / 1.82;
 }
 
 uint64_t    perft(board_t *board, unsigned int depth)
