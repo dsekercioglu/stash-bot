789f0c55d6a61f720ce8e913415eb0ef3f47219d
Morgan Houppin <morganhouppin@gmail.com>
2021-01-29 15:51:14 +0100

    Tuning at 20+0.2, 32 epochs Bench: 27049609

--- src/sources/engine/engine_go.c
+++ src/sources/engine/engine_go.c
@@ -34,7 +34,7 @@ void        init_reduction_table(void)
 {
     for (int d = 1; d < 64; ++d)
         for (int m = 1; m < 64; ++m)
-            Reductions[d][m] = -0.31 + log(d) * log(m) / 1.49;
+            Reductions[d][m] = -0.48 + log(d) * log(m) / 3.57;
 }
 
 uint64_t    perft(board_t *board, unsigned int depth)
