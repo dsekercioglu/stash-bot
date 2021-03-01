efc7bd155b6e6fa1d2f75e21fca081102c83d7f0
Morgan Houppin <morganhouppin@gmail.com>
2021-03-01 18:59:38 +0100

    Switched current KS scaling in middlegame to quadratic Bench: 14446506

--- src/sources/engine/evaluate.c
+++ src/sources/engine/evaluate.c
@@ -31,10 +31,11 @@ enum
 
     // King Safety eval terms
 
-    KnightWeight = SPAIR(26, 8),
-    BishopWeight = SPAIR(18, 5),
-    RookWeight = SPAIR(51, -4),
-    QueenWeight = SPAIR(51, 72),
+    KnightWeight = SPAIR(21, 0),
+    BishopWeight = SPAIR(20, -3),
+    RookWeight = SPAIR(42, -3),
+    QueenWeight = SPAIR(68, 23),
+    KS_Offset = SPAIR(23, -5),
 
 	// Knight eval terms
 
@@ -91,10 +92,6 @@ const scorepair_t   MobilityQ[28] = {
     SPAIR(  29, 191), SPAIR(  25, 197), SPAIR(  24, 198), SPAIR(  21, 196),
     SPAIR(  33, 186), SPAIR(  33, 183), SPAIR(  31, 171), SPAIR(  31, 167),
     SPAIR(  21, 156), SPAIR(  15, 151), SPAIR(  15, 145), SPAIR(  17, 146)
-};
-
-const int   AttackRescale[8] = {
-    0, 0, 2, 4, 8, 16, 32, 64
 };
 
 typedef struct
@@ -409,12 +406,11 @@ scorepair_t evaluate_safety(evaluation_t *eval, color_t c)
 
     if (eval->attackers[c] >= 2)
     {
-        scorepair_t bonus = eval->weights[c];
+        scorepair_t bonus = eval->weights[c] + KS_Offset;
 
-        if (eval->attackers[c] < 8)
-            bonus -= scorepair_divide(bonus, AttackRescale[eval->attackers[c]]);
+        score_t mg = midgame_score(bonus), eg = endgame_score(bonus);
 
-        return (bonus);
+        return (create_scorepair(max(mg, 0) * mg / 512, max(eg, 0) / 32));
     }
     return (0);
 }
