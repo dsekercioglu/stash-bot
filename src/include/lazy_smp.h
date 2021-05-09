6f7d24b0b121000281d45fd4252212250ff0191f
Morgan Houppin <morganhouppin@gmail.com>
2021-05-09 14:15:23 +0200

    Make draw score use the 2 lowest bits of the node counter Bench: 14551504

--- src/include/lazy_smp.h
+++ src/include/lazy_smp.h
@@ -88,7 +88,7 @@ INLINED uint64_t get_node_count(void)
 
 INLINED score_t draw_score(const worker_t *worker)
 {
-    return ((worker->nodes & 1) - 0);
+    return ((worker->nodes & 3) - 1);
 }
 
 void wpool_init(int threads);
