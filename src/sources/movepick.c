cd1a1e4458a88d81086259cafb9f010378aeb594
Morgan Houppin <morganhouppin@gmail.com>
2021-06-05 17:14:38 +0200

    Reduced slightly VictimBonus values Bench: 12221722

--- src/sources/movepick.c
+++ src/sources/movepick.c
@@ -54,7 +54,7 @@ void movepick_init(movepick_t *mp, bool inQsearch, const board_t *board,
 static void score_captures(movepick_t *mp, extmove_t *begin, extmove_t *end)
 {
     static const score_t VictimBonus[PIECETYPE_NB] = {
-        0, 0, 1024, 1024, 2048, 4096, 0, 0
+        0, 0, 768, 768, 1536, 3072, 0, 0
     };
 
     while (begin < end)
