cf7e8c91a1511004485aa573ab1eedcfb815bee2
Morgan Houppin <morganhouppin@gmail.com>
2021-08-07 10:36:46 +0200

    Change Futility Pruning behaviour in qsearch
    Rename Delta Pruning to Futility Pruning, change the threshold to 100 cp,
    and adjust bestScore value before pruning the move.
    
    Bench: 10,556,238

--- src/sources/search.c
+++ src/sources/search.c
@@ -464,10 +464,10 @@ score_t qsearch(board_t *board, score_t alpha, score_t beta, searchstack_t *ss, 
     if (pvNode)
         (ss + 1)->pv = pv;
 
-    // Check if delta pruning is possible.
+    // Check if Futility Pruning is possible.
 
-    const bool deltaPruning = (!inCheck && popcount(board->piecetypeBB[ALL_PIECES]) > 6);
-    const score_t deltaBase = bestScore + 200;
+    const bool futilityPruning = (!inCheck && popcount(board->piecetypeBB[ALL_PIECES]) > 6);
+    const score_t futilityBase = bestScore + 100;
 
     while ((currmove = movepick_next_move(&mp, false)) != NO_MOVE)
     {
@@ -483,14 +483,17 @@ score_t qsearch(board_t *board, score_t alpha, score_t beta, searchstack_t *ss, 
 
         bool givesCheck = move_gives_check(board, currmove);
 
-        if (bestScore > -MATE_FOUND && deltaPruning && !givesCheck && move_type(currmove) == NORMAL_MOVE)
+        if (bestScore > -MATE_FOUND && futilityPruning && !givesCheck && move_type(currmove) == NORMAL_MOVE)
         {
-            score_t delta = deltaBase + PieceScores[ENDGAME][piece_on(board, to_sq(currmove))];
+            score_t futilityValue = futilityBase + PieceScores[ENDGAME][piece_on(board, to_sq(currmove))];
 
             // Check if the move is very unlikely to improve alpha.
 
-            if (delta < alpha)
+            if (futilityValue < alpha)
+            {
+                bestScore = max(bestScore, futilityValue);
                 continue ;
+            }
         }
 
         ss->currentMove = currmove;
