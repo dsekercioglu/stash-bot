4b228e7a5258468272f1b4c051189d9844cc4327
Morgan Houppin <morganhouppin@gmail.com>
2021-08-31 09:45:11 +0200

    Tuning at 20+0.2, 113 epochs
    Bench: 27,483,557

--- src/sources/engine.c
+++ src/sources/engine.c
@@ -29,13 +29,16 @@
 #include "tt.h"
 #include "uci.h"
 
-int Reductions[64][64];
+int Reductions[2][64][64];
 
 void init_reduction_table(void)
 {
     for (int d = 1; d < 64; ++d)
         for (int m = 1; m < 64; ++m)
-            Reductions[d][m] = -1.34 + log(d) * log(m) / 1.26;
+        {
+            Reductions[0][d][m] = -1.23 + log(d) * log(m) / 2.88;
+            Reductions[1][d][m] = -1.22 + log(d) * log(m) / 2.07;
+        }
 }
 
 uint64_t perft(board_t *board, unsigned int depth)
