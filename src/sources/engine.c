eaa93333e83aa051da9bc22fbb005dfa914a5db1
Morgan Houppin <morganhouppin@gmail.com>
2021-12-24 23:23:52 +0100

    Tuning at 160 knodes/move, 200 epochs
    Bench: 6,498,044

--- src/sources/engine.c
+++ src/sources/engine.c
@@ -36,8 +36,8 @@ void init_reduction_table(void)
     for (int d = 1; d < 64; ++d)
         for (int m = 1; m < 64; ++m)
         {
-            Reductions[0][d][m] = -0.95 + log(d) * log(m) / 2.15;
-            Reductions[1][d][m] = -0.55 + log(d) * log(m) / 1.40;
+            Reductions[0][d][m] = -0.45 + log(d) * log(m) / 1.95;
+            Reductions[1][d][m] = -0.45 + log(d) * log(m) / 1.45;
         }
 
     for (int d = 1; d < 7; ++d)
