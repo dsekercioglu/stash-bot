b31dd7abe87859d0493334729e603232f0f7178d
Morgan Houppin <morganhouppin@gmail.com>
2022-02-14 20:44:37 +0100

    Retune SEE constants after changing material values
    Tuning at 200knpm, 40 epochs
    
    Bench: 8,914,769

--- src/sources/search.c
+++ src/sources/search.c
@@ -244,7 +244,7 @@ __main_loop:
 
             // SEE Pruning.
 
-            if (depth <= 5 && !see_greater_than(board, currmove, (isQuiet ? -80 * depth : -25 * depth * depth)))
+            if (depth <= 5 && !see_greater_than(board, currmove, (isQuiet ? -64 * depth : -13 * depth * depth)))
                 continue ;
         }
 
