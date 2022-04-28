b41625cfaa35873dbddaef5ca4224472524a9341
Morgan Houppin <morganhouppin@gmail.com>
2022-04-28 11:09:22 +0200

    Use stricter criteria for reducing non-quiet moves
    Bench: 7,675,639

--- src/sources/search.c
+++ src/sources/search.c
@@ -322,7 +322,7 @@ __main_loop:
                 R = clamp(R, 0, newDepth - 1);
             }
             else
-                R = 1;
+                R = !givesCheck && mp.stage == PICK_BAD_INSTABLE;
         }
         else
             R = 0;
