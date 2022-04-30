ea3de521aabd6f10cc560934cd63d6f2ec827d72
Morgan Houppin <morganhouppin@gmail.com>
2022-04-30 15:56:36 +0200

    Tuning at 200knpm, 196 epochs
    Bench: 7,636,454

--- src/sources/search.c
+++ src/sources/search.c
@@ -163,7 +163,7 @@ score_t search(board_t *board, int depth, score_t alpha, score_t beta, searchsta
     {
         boardstack_t stack;
 
-        int R = 4 + min((eval - beta) / 166, 2) + (depth / 4);
+        int R = 4 + min((eval - beta) / 177, 1) + (depth / 5);
 
         ss->currentMove = NULL_MOVE;
         ss->pieceHistory = NULL;
@@ -181,7 +181,7 @@ score_t search(board_t *board, int depth, score_t alpha, score_t beta, searchsta
 
             // Do not trust win claims.
 
-            if (worker->verifPlies || (depth <= 10 && abs(beta) < VICTORY))
+            if (worker->verifPlies || (depth <= 9 && abs(beta) < VICTORY))
                 return (score);
 
             // Zugzwang checking.
